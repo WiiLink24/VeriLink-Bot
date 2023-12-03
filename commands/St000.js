c5b4d3a503114de981fe7700259351f220042fbf
Matthew <github-gpg@service.matthe815.dev>
2023-12-03 18:38:56 -0500

    fix: Fix text error in st000 command

--- commands/St000.js
+++ commands/St000.js
@@ -7,10 +7,10 @@ module.exports = {
     .setDefaultMemberPermissions(0x2000),
   async execute (interaction) {
     interaction.reply(`This error means that the server you are connecting to does not have the requested quest file for your quest. This is not an issue with Erupe, but rather the server you are connecting to.
-    If you are the server owner, please extract the quests onto the bin folder of your server, linked on the official Erupe repository: https://github.com/zerulight/erupe
+If you are the server owner, please extract the quests onto the bin folder of your server, linked on the official Erupe repository: https://github.com/zerulight/erupe
 
-    このエラーは、接続しているサーバーがクエストファイルを持っていないことを意味します。これはErupeの問題ではなく、接続しているサーバーの問題です。
-    サーバーのオーナーの場合、ErupeのGitHubからにクエストをダウンロードしてサーバーのbinに入れてください。 クエストが https://github.com/zerulight/erupe からです
+このエラーは、接続しているサーバーがクエストファイルを持っていないことを意味します。これはErupeの問題ではなく、接続しているサーバーの問題です。
+サーバーのオーナーの場合、ErupeのGitHubからにクエストをダウンロードしてサーバーのbinに入れてください。 クエストが https://github.com/zerulight/erupe からです
     `)
   }
 }
