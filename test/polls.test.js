7944ef00f68bd416d721c131482a1baa1936d9b7
Matthew <github-gpg@service.matthe815.dev>
2023-12-03 14:46:39 -0500

    fix: Move database under condition

--- test/polls.test.js
+++ test/polls.test.js
@@ -11,57 +11,57 @@ const sampleData = {
   title: 'Test Title'
 }
 
-const database = new Database()
-database.Connect().then(() => {
-  describe('Polls', () => {
-    describe('unpublished', () => {
-      const poll = (new Poll(null, sampleData))
-      it('should have the title \'Test Title\'', () => {
-        assert.equal(poll.title, 'Test Title')
-      })
-      it('should not have a channel_id', () => {
-        assert.equal(poll.channel_id, '')
-      })
-      it('should not have a message_id', () => {
-        assert.equal(poll.message_id, '')
-      })
-      it('should not be published', () => {
-        assert.equal(poll.is_published, false)
-      })
-      it('ID should not equal 0', () => {
-        assert.notEqual(poll.id, 0)
-      })
+describe('Polls', () => {
+  describe('unpublished', () => {
+    const poll = (new Poll(null, sampleData))
+    it('should have the title \'Test Title\'', () => {
+      assert.equal(poll.title, 'Test Title')
     })
-    describe('published', () => {
-      const poll = (new Poll(null, sampleData))
-      poll.channel_id = '123'
-      poll.message_id = '123'
-      poll.is_published = true
-      it('should have channel_id', () => {
-        assert.notEqual(poll.channel_id, '')
-      })
-      it('should have message_id', () => {
-        assert.notEqual(poll.message_id, '')
-      })
-      it('should be published', () => {
-        assert.equal(poll.is_published, true)
-      })
+    it('should not have a channel_id', () => {
+      assert.equal(poll.channel_id, '')
     })
-    describe('#AddOption', () => {
-      const poll = (new Poll(null, sampleData))
-      const err = poll.AddOption('Test')
-      it('should not return a value', () => {
-        assert.equal(err, null)
-      })
-      it('length of options should be 1', () => {
-        assert.equal(poll.options.length, 1)
-      })
-      it('first value in options should be \'Test\'', () => {
-        assert.equal(poll.options[0], 'Test')
-      })
+    it('should not have a message_id', () => {
+      assert.equal(poll.message_id, '')
     })
-    // Don't do database stuff without config
-    if (!fs.existsSync('../config/config.json')) return
+    it('should not be published', () => {
+      assert.equal(poll.is_published, false)
+    })
+    it('ID should not equal 0', () => {
+      assert.notEqual(poll.id, 0)
+    })
+  })
+  describe('published', () => {
+    const poll = (new Poll(null, sampleData))
+    poll.channel_id = '123'
+    poll.message_id = '123'
+    poll.is_published = true
+    it('should have channel_id', () => {
+      assert.notEqual(poll.channel_id, '')
+    })
+    it('should have message_id', () => {
+      assert.notEqual(poll.message_id, '')
+    })
+    it('should be published', () => {
+      assert.equal(poll.is_published, true)
+    })
+  })
+  describe('#AddOption', () => {
+    const poll = (new Poll(null, sampleData))
+    const err = poll.AddOption('Test')
+    it('should not return a value', () => {
+      assert.equal(err, null)
+    })
+    it('length of options should be 1', () => {
+      assert.equal(poll.options.length, 1)
+    })
+    it('first value in options should be \'Test\'', () => {
+      assert.equal(poll.options[0], 'Test')
+    })
+  })
+  // Don't do database stuff without config
+  if (!fs.existsSync('../config/config.json')) return
+  const database = new Database()
+  database.Connect().then(() => {
     describe('#Save', () => {
       const poll = (new Poll(null, sampleData))
       poll.client = { db: database }
