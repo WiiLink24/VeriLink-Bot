07e9bb9cae11a42e91e967c6d4c259bc4a646067
Matthew <github-gpg@service.matthe815.dev>
2023-12-09 20:49:39 -0500

    docs: Add header to Vote

--- src/Vote.js
+++ src/Vote.js
@@ -1,3 +1,7 @@
+/**
+ * Represents a vote from a user.
+ * This can be any combination of a member + option
+ */
 export default class Vote {
   constructor (member, option) {
     this.member = member
