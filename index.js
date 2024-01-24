c8fbbc984defeaaa624aada2a1b507135c4ce9d2
bennyman123abc <bennyman123abc@hotmail.com>
2024-01-24 00:08:19 -0600

    Cleanup unused code & implement Express server

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import Discord from 'discord.js'
-import PartnyaClient from './src/PartnyaClient.js'
+import VeriLinkClient from './src/VeriLinkClient.js'
 import { Logger } from './src/Logger.js'
 import fs from 'node:fs'
+import express from 'express'
 
 const config = JSON.parse(String(fs.readFileSync('./config/config.json')))
 const flags = process.argv.length > 2 ? process.argv[2] : ''
-const client = new PartnyaClient({ intents: [Discord.IntentsBitField.Flags.Guilds] })
+const client = new VeriLinkClient({ intents: [Discord.IntentsBitField.Flags.Guilds] })
+const app = express()
 
 client.on(Discord.Events.ClientReady, async _ => {
-  // Load client data (Commands, Polls, etc)
+  // Load client data
   await client.load()
   Logger.info(`Client logged in as user: ${client.user.tag}!`)
 })
@@ -46,7 +48,7 @@ client.on(Discord.Events.InteractionCreate, async interaction => {
   }
 })
 
-await client.db.Connect()
+if (client.db) await client.db.Connect()
 
 if (flags === '-migrate') {
   Logger.info('Starting database migration...')
@@ -55,5 +57,8 @@ if (flags === '-migrate') {
   Logger.info('Database migration has completed.')
   process.exit() // Exit once migration is complete
 } else {
-  await client.login(config.token)
+  // await client.login(config.token)
+  app.listen(config.api.port, () => {
+    Logger.info(`REST API listening on port ${config.api.port}`);
+  })
 }
--- src/VeriLinkClient.js
+++ src/VeriLinkClient.js
@@ -1,16 +1,18 @@
 import Discord, { Client } from 'discord.js'
 import Database from './Database.js'
-import Poll from './Poll.js'
 import { Logger } from './Logger.js'
+import fs from 'fs'
 
-export default class PartnyaClient extends Client {
+const config = JSON.parse(String(fs.readFileSync('./config/config.json')))
+
+export default class VeriLinkClient extends Client {
   constructor (data) {
     super(data)
-    this.db = new Database()
+    if (config.database.enabled) this.db = new Database()
   }
 
   async load () {
-    Logger.info('Loading PartnyaClient')
+    Logger.info('Loading VeriLinkClient')
     Logger.info('Initalizing commands')
     await this.rest.put(Discord.Routes.applicationCommands(this.user.id), { body: this.commands.all().map((command) => command.data) })
     Logger.info(`Commands loaded (${this.commands.all().length})`)
