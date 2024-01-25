da971f5e84a48d38f986af9108bfbbb001649089
Matthew <github-gpg@service.matthe815.dev>
2024-01-25 00:34:23 -0500

    feat: Discord token validation

--- src/WebHost/WebHost.js
+++ src/WebHost/WebHost.js
@@ -24,7 +24,27 @@ export default class WebHost {
   }
 
   initializeEndpoints () {
-    this.app.post('/api/captcha', async (req, res, next) => {
+    this.app.post('/api/token', async (req, res) => {
+      const token = req.body.code
+
+      // Parameters for token request
+      const params = new URLSearchParams()
+      params.append('grant_type', 'authorization_code')
+      params.append('code', token)
+      params.append('redirect_uri', config.api.redirectUri)
+      params.append('client_id', config.api.clientId)
+      params.append('client_secret', config.api.clientSecret)
+
+      const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+
+      if (tokenResponse.data.error) res.status(403).send({ success: false, message: 'Token failed to authenticate.' })
+
+      const userResponse = await axios.get('https://discord.com/api/users/@me', { headers: { Authorization: `Bearer ${tokenResponse.data.access_token}` } })
+
+      res.status(200).send({ success: true, token: tokenResponse.data.access_token, data: userResponse.data })
+    })
+
+    this.app.post('/api/captcha', async (req, res) => {
       const token = req.body.token
       const captchaRes = await axios.get(`https://www.google.com/recaptcha/api/siteverify?secret=${config.api.captchaSecret}&response=${token}`)
 
