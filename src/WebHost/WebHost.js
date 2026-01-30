518211cccb34bfb40ea9942c7a5de50ab60eeb28
Matthew <github-gpg@service.matthe815.dev>
2026-01-30 13:21:59 -0500

    Update WebHost.js

--- src/WebHost/WebHost.js
+++ src/WebHost/WebHost.js
@@ -29,6 +29,7 @@ export default class WebHost {
   initializeEndpoints () {
     this.app.post('/api/token', async (req, res) => {
       const { code } = req.body
+      console.log(req.headers)
       const ip = req.headers['cf-connecting-ip']
       if (!(code || typeof (code) === 'string')) return res.status(402).send({ success: false, message: 'Request malformed.' })
 
